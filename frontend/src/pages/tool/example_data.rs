130b7cce5aced12194858db12a3cf69e897c6664
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-11-02 15:27:02 +0100

    Move example data into JSON file

--- frontend/src/pages/tool/example_data.rs
+++ frontend/src/pages/tool/example_data.rs
@@ -132,51 +132,13 @@ pub fn load_example_field_signal_values(signals: &HashMap<ValueId, FieldSignal>)
         .set(Some(util::n2o_szenario_to_usize(n2o_szenario)));
 }
 
+const EXAMPLE_DATA: &[u8] = include_bytes!("example_data.json");
+
 fn example_input_data() -> InputData {
-    let ew = 120_000.0;
-    let abwasser = 5_000_000.0;
     // TODO: let csb_zu = 1045.0;
-    let n_ges_zu = 122.0;
     // TODO: let p_zu = 9.9;
-    let csb_ab = 129.0;
-    let n_ges_ab = 11.76;
     // TODO: let p_ab = 0.4;
-    let klaergas_gesamt = 1_260_000.0;
-    let methangehalt = 23.0;
     // TODO: let gas_zusatz = 1_300_000.0;
     // TODO: let biogas = false;
-    let strombedarf = 2_683_259.0;
-    let energie_eigen = 2_250_897.0;
-    let ef_co2_strommix = 468.0;
-    let schlammtaschen = true;
-    let schlammstapel = true;
-    let klaerschlamm_entsorgung_m = 3687.6;
-    let klaerschlamm_transport_km = 47.0;
-    let betriebsstoffe_fe3 = 0.0;
-    let betriebsstoffe_feso4 = 326_000.0;
-    let betriebsstoffe_kalk = 326_260.0;
-    let betriebsstoffe_poly = 23_620.0;
-    let n2o_szenario = N2OSzenario::Ipcc2019;
-
-    InputData {
-        ew,
-        abwasser,
-        n_ges_zu,
-        csb_ab,
-        n_ges_ab,
-        klaergas_gesamt,
-        methangehalt,
-        strombedarf,
-        energie_eigen,
-        ef_co2_strommix,
-        schlammtaschen,
-        schlammstapel,
-        klaerschlamm_transport_km,
-        klaerschlamm_entsorgung_m,
-        betriebsstoffe_fe3,
-        betriebsstoffe_feso4,
-        betriebsstoffe_kalk,
-        betriebsstoffe_poly,
-        n2o_szenario,
-    }
+    serde_json::from_slice(EXAMPLE_DATA).unwrap()
 }
