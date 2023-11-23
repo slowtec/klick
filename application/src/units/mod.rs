629b01b44386a9d30e5a77dbd60a697724c2bfad
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-11-23 21:50:34 +0100

    Use macro to create units

--- application/src/units/mod.rs
+++ application/src/units/mod.rs
@@ -1,6 +1,30 @@
-mod factor;
-mod kilometers;
-mod percent;
-mod tons;
+macro_rules! unit {
 
-pub use self::{factor::*, kilometers::*, percent::*, tons::*};
+    ($name:ident, $abbreviation:expr, $description:expr) => {
+
+        #[doc = concat!($description, "`[",$abbreviation, "].`")]
+        #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+        pub struct $name(f64);
+
+        impl $name {
+            #[must_use]
+            pub const fn new(value: f64) -> Self {
+                Self(value)
+            }
+        }
+
+        impl From<$name> for f64 {
+            fn from(from: $name) -> Self {
+                from.0
+            }
+        }
+    };
+}
+
+unit!(Kilometers, "km", "Kilometers");
+unit!(Tons, "t", "Tons");
+
+mod factor;
+mod percent;
+
+pub use self::{factor::*, percent::*};
