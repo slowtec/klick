ff9bcafd4312f838c4f361d3c4ce30cbbc300e7e
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-11-28 01:21:38 +0100

    Simplify sankey chart

--- crates/svg-charts/examples/sankey/src/main.rs
+++ crates/svg-charts/examples/sankey/src/main.rs
@@ -9,24 +9,26 @@ fn MySankeyChart() -> impl IntoView {
     let a = sankey.node(5.0, Some("a"));
     let b = sankey.node(2.0, Some("b"));
 
-
     let e = sankey.node(5.0, Some("e"));
     let d = sankey.node(5.0, Some("d"));
-    let g = sankey.node(10.0, Some("g"));
-    let h = sankey.node(15.0, Some("h"));
+    let g = sankey.node(7.0, Some("g"));
+    let h = sankey.node(8.0, Some("h"));
     let i = sankey.node(10.0, Some("i"));
     let j = sankey.node(5.0, Some("j"));
-    let k = sankey.node(25.0, Some("k"));
+    let k = sankey.node(23.0, Some("k"));
     let f = sankey.node(5.0, Some("f"));
 
     sankey.edge(a, g);
     sankey.edge(b, g);
+
     sankey.edge(c, h);
+    sankey.edge(g, h);
+
     sankey.edge(d, i);
     sankey.edge(e, i);
+
     sankey.edge(f, j);
 
-    sankey.edge(g, h);
     sankey.edge(h, k);
     sankey.edge(i, k);
     sankey.edge(j, k);
@@ -34,9 +36,14 @@ fn MySankeyChart() -> impl IntoView {
     view! {
       <h1>"Sankey Chart Example"</h1>
       <SankeyChart
+        sankey = { sankey.clone() }
+        width = 800.0
+        height = 300.0
+      />
+      <SankeyChart
         sankey
         width = 800.0
-        height = 400.0
+        height = 300.0
       />
     }
 }
--- crates/svg-charts/src/sankey.rs
+++ crates/svg-charts/src/sankey.rs
@@ -79,18 +79,21 @@ pub fn Chart(sankey: Sankey, width: f64, height: f64) -> impl IntoView {
     let nodes = node_positions
         .iter()
         .map(|(id, (x, y, node_height))| {
-
-           let font_size = height / 40.0;
+            let font_size = height / 40.0;
             let value = sankey.nodes[id.0].value;
-            let label = sankey.nodes[id.0].label.as_ref().map(|label|
-              view! {
-                <tspan>
-                  { label }
-                </tspan>
-              }
-            );
+            let label = sankey.nodes[id.0].label.as_ref().map(|label| {
+                view! {
+                  <tspan>
+                    { label }
+                  </tspan>
+                }
+            });
 
-            let value_dx = if label.is_some(){ font_size/4.0 } else { 0.0 };
+            let value_dx = if label.is_some() {
+                font_size / 4.0
+            } else {
+                0.0
+            };
 
             view! {
               <rect
@@ -100,8 +103,8 @@ pub fn Chart(sankey: Sankey, width: f64, height: f64) -> impl IntoView {
                 height = { *node_height }
                 fill = "#555"
               />
-              <text 
-                x = {*x + node_width + font_size/2.0 } 
+              <text
+                x = {*x + node_width + font_size/2.0 }
                 y = {*y + node_height /2.0 }
                 fill = "#111"
                 text-anchor = "start"
@@ -252,82 +255,67 @@ fn node_positions(
         .iter()
         .enumerate()
         .flat_map(|(i, layer)| {
-
             let x = layer_positions[i];
             let mut y = 0.0;
             layer
                 .iter()
                 .map(|id| {
-                    let gap_count = gap_count(deps, id);
-
                     let node_y = y;
 
-                    let v = if deps[id].inputs.is_empty() {
-                        nodes[id.0].value
-                    } else {
-                        input_value(deps, nodes, id)
-                    };
+                    let v = nodes[id.0].value;
 
-                    let node_height = v * scale + gap_count as f64 * gap;
+                    let node_height = v * scale;
 
                     y += node_height + gap;
                     (*id, (x, node_y, node_height))
                 })
                 .collect::<Vec<_>>()
         })
-        .collect::<HashMap<_,_>>();
+        .collect::<HashMap<_, _>>();
 
-  for layer in layers.iter().rev() {
-    for id in layer {
+    for layer in layers.iter().rev() {
+        for id in layer {
+            if deps[id].inputs.is_empty() {
+                continue;
+            };
 
-      if deps[id].inputs.is_empty() {
-        continue;
-      };
-    
-      let min_y = deps[id].inputs.iter().map(|id| positions[id].1)
-          .fold(f64::INFINITY, |min, val| if val < min { val } else { min });
+            let min_y = deps[id]
+                .inputs
+                .iter()
+                .map(|id| positions[id].1)
+                .fold(f64::INFINITY, |min, val| if val < min { val } else { min });
 
-      let self_y = positions[id].1;
+            let self_y = positions[id].1;
 
-      if self_y < min_y {
-        positions.get_mut(id).unwrap().1 = min_y;
-      }
-
+            if self_y < min_y {
+                positions.get_mut(id).unwrap().1 = min_y;
+            }
+        }
     }
-  }
-  positions
-}
-
-fn gap_count(deps: &HashMap<NodeId, Dependencies>, id: &NodeId) -> usize {
-    let input_count = deps[id].inputs.len();
-
-    let gaps = if input_count > 0 { input_count - 1 } else { 0 };
-
-    gaps + deps[id]
-        .inputs
-        .iter()
-        .map(|id| gap_count(deps, id))
-        .fold(0, |x, v| x + v)
-}
-
-fn input_value(deps: &HashMap<NodeId, Dependencies>, nodes: &[Node], id: &NodeId) -> f64 {
-    deps[id]
-        .inputs
-        .iter()
-        .map(|id| nodes[id.0].value)
-        .fold(0.0, |x, v| x + v)
+    positions
 }
 
 fn scale(layers: &[Vec<NodeId>], nodes: &[Node], height: f64, node_separation: f64) -> f64 {
-    layers
-        .iter()
-        .map(|layer| {
-            let total_value = layer.iter().fold(0.0, |x, id| x + nodes[id.0].value);
-            let count = layer.len() as f64;
-            let scale = (height - node_separation * count) / total_value;
-            scale
-        })
-        .fold(f64::INFINITY, |min, val| if val < min { val } else { min })
+    let (max_node_count, layer_val) = layers.iter().fold((usize::MIN, 0.0), |(cnt, val), layer| {
+        if layer.len() > cnt {
+            (
+                layer.len(),
+                layer.iter().fold(0.0, |x, id| x + nodes[id.0].value),
+            )
+        } else {
+            (cnt, val)
+        }
+    });
+
+    let max_gaps = if max_node_count > 0 {
+        max_node_count - 1
+    } else {
+        0
+    } as f64;
+
+    let scale = (height - max_gaps * node_separation) / layer_val;
+
+    scale
 }
 
 struct Point {
