bf925c672358a6d8c6459e215081ddaa7a4ba134
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-01-24 15:43:58 +0100

    Fix SQL migration

--- crates/db-sqlite/migrations/2024-01-24-000000_account_creation_date/up.sql
+++ crates/db-sqlite/migrations/2024-01-24-000000_account_creation_date/up.sql
@@ -1,2 +1,51 @@
+PRAGMA foreign_keys=off;
+
+-- We don't need to start a transaction here
+-- because diesel executes the migrations
+-- in a transaction anyway :)
+
 ALTER TABLE accounts
 ADD COLUMN created_at INTEGER NOT NULL DEFAULT 0;
+
+-- Added 'ON DELETE CASCADE'
+-- to the foreign key 'account_rowid'
+CREATE TABLE account_tokens_new (
+    rowid         INTEGER NOT NULL,
+    account_rowid INTEGER NOT NULL,
+    expires_at    INTEGER NOT NULL,
+    nonce         TEXT    NOT NULL,
+    --
+    UNIQUE        (account_rowid),
+    UNIQUE        (nonce),
+    --
+    PRIMARY KEY   (rowid),
+    FOREIGN KEY   (account_rowid) REFERENCES accounts(rowid) ON DELETE CASCADE
+);
+
+-- Added 'ON DELETE CASCADE'
+-- to the foreign key 'account_rowid'
+CREATE TABLE projects_new (
+    rowid         INTEGER NOT NULL,
+    project_id    TEXT    NOT NULL,
+    account_rowid INTEGER NOT NULL,
+    data          JSON    NOT NULL, -- JSON serialized project data
+    --
+    UNIQUE        (project_id),
+    --
+    PRIMARY KEY   (rowid),
+    FOREIGN KEY   (account_rowid) REFERENCES accounts(rowid) ON DELETE CASCADE
+);
+
+INSERT INTO account_tokens_new
+SELECT * FROM account_tokens;
+
+INSERT INTO projects_new
+SELECT * FROM projects;
+
+DROP TABLE account_tokens;
+ALTER TABLE account_tokens_new RENAME TO account_tokens;
+
+DROP TABLE projects;
+ALTER TABLE projects_new RENAME TO projects;
+
+PRAGMA foreign_keys=on;
---
+++ crates/db-sqlite/tests/accounts.rs
@@ -0,0 +1,82 @@
+use time::{Duration, OffsetDateTime};
+
+use klick_application::{AccountRecord, AccountRepo, AccountTokenRepo, ProjectRepo};
+use klick_db_sqlite::Connection;
+use klick_domain::{Project, Account, AccountToken, EmailNonce, Nonce, Password, ProjectId};
+use klick_boundary::ProjectData;
+
+#[test]
+fn delete_outdated_unconfirmed_accounts() {
+    let db = Connection::establish(":memory:").unwrap();
+    db.run_embedded_database_migrations().unwrap();
+
+    let now = OffsetDateTime::now_utc();
+
+    let new_account = AccountRecord {
+        account: Account {
+            email_address: "new@bar.baz".parse().unwrap(),
+            email_confirmed: false,
+            created_at: now,
+        },
+        password: "very-secret".parse::<Password>().unwrap().to_hashed(),
+    };
+
+    let outdated_account = AccountRecord {
+        account: Account {
+            email_address: "outdated@bar.baz".parse().unwrap(),
+            email_confirmed: false,
+            created_at: now - Duration::days(2),
+        },
+        password: "very-secret".parse::<Password>().unwrap().to_hashed(),
+    };
+    db.save_account(&new_account).unwrap();
+    db.save_account(&outdated_account).unwrap();
+
+    assert!(db
+        .find_account(&"new@bar.baz".parse().unwrap())
+        .unwrap()
+        .is_some());
+    assert!(db
+        .find_account(&"outdated@bar.baz".parse().unwrap())
+        .unwrap()
+        .is_some());
+
+
+    // -- add account token
+    let email_nonce = EmailNonce {
+        email: outdated_account.account.email_address.clone(),
+        nonce: Nonce::new(),
+    };
+    let expires_at = OffsetDateTime::now_utc() + Duration::hours(5);
+    let token = AccountToken {
+        email_nonce,
+        expires_at,
+    };
+    db.replace_account_token(token).unwrap();
+
+    // -- add project
+    let id = ProjectId::new();
+    let created_at = OffsetDateTime::now_utc();
+    let modified_at = None;
+    let data = ProjectData::default();
+    let project = Project {
+        id,
+        created_at,
+        modified_at,
+        data,
+    };
+    db.save_project(project, &outdated_account.account.email_address).unwrap();
+
+    // -- delete outated
+    let created_before = now - Duration::days(2);
+    db.delete_old_unconfirmed_accounts(created_before).unwrap();
+
+    assert!(db
+        .find_account(&"new@bar.baz".parse().unwrap())
+        .unwrap()
+        .is_some());
+    assert!(db
+        .find_account(&"outdated@bar.baz".parse().unwrap())
+        .unwrap()
+        .is_none());
+}
