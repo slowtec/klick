5949986afd4291dfaeaf4c582b433fa04b16991e
Joachim Schiele <js@lastlog.de>
2024-02-14 13:11:52 +0000

    #139 Handlungsempfehlungen boxes - change order

---
+++ frontend/src/pages/tool/optimization_options/fossil_co2_emissions.rs
@@ -0,0 +1,13 @@
+use leptos::*;
+
+use super::{Card, Cite, DWA_MERKBLATT_URL};
+
+pub fn options() -> impl IntoView {
+    view! {
+      <Card title = "Fossile CO₂-Emissionen aus Abwasser">
+        <p>
+          "xxx"
+        </p>
+      </Card>
+    }
+}
