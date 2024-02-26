018f0782219924dbab1e994b8792f05f254138b6
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-02-26 16:00:55 +0100

    Publish klick-domain

--- build.rs
+++ build.rs
@@ -1,16 +1,27 @@
 use std::fs;
 
 fn main() {
-    let backend = read_cargo_toml("Cargo.toml");
-    let frontend = read_cargo_toml("frontend/Cargo.toml");
+    let workspace_cargo_toml = read_cargo_toml("Cargo.toml");
+    let frontend_cargo_toml = read_cargo_toml("frontend/Cargo.toml");
 
-    let backend_version = get_version(&backend);
-    let frontend_version = get_version(&frontend);
+    let frontend_version = get_package_version(&frontend_cargo_toml).as_str().unwrap();
+
+    let backend_uses_workspace_version = get_package_version(&workspace_cargo_toml)
+        .get("workspace")
+        .unwrap()
+        .as_bool()
+        .unwrap();
+
+    assert!(backend_uses_workspace_version);
+
+    let workspace_version = get_package_version(&workspace_cargo_toml.get("workspace").unwrap())
+        .as_str()
+        .unwrap();
 
     // Check if versions are the same
     assert_eq!(
-        backend_version, frontend_version,
-        "Versions are not the same: backend = {backend_version} vs frontend = {frontend_version}"
+        workspace_version, frontend_version,
+        "Versions are not the same: workspace = {workspace_version} vs. frontend = {frontend_version}"
     );
 }
 
@@ -18,12 +29,6 @@ fn read_cargo_toml(path: &str) -> toml::Value {
     fs::read_to_string(path).unwrap().parse().unwrap()
 }
 
-fn get_version(cargo_toml: &toml::Value) -> &str {
-    cargo_toml
-        .get("package")
-        .unwrap()
-        .get("version")
-        .unwrap()
-        .as_str()
-        .unwrap()
+fn get_package_version(cargo_toml: &toml::Value) -> &toml::Value {
+    cargo_toml.get("package").unwrap().get("version").unwrap()
 }
--- frontend/build.rs
+++ frontend/build.rs
@@ -1,16 +1,18 @@
 use std::fs;
 
 fn main() {
-    let backend = read_cargo_toml("../Cargo.toml");
-    let frontend = read_cargo_toml("Cargo.toml");
+    let workspace_cargo_toml = read_cargo_toml("../Cargo.toml");
+    let frontend_cargo_toml = read_cargo_toml("Cargo.toml");
 
-    let backend_version = get_version(&backend);
-    let frontend_version = get_version(&frontend);
+    let frontend_version = get_package_version(&frontend_cargo_toml).as_str().unwrap();
+    let workspace_version = get_package_version(&workspace_cargo_toml.get("workspace").unwrap())
+        .as_str()
+        .unwrap();
 
     // Check if versions are the same
     assert_eq!(
-        backend_version, frontend_version,
-        "Versions are not the same: backend = {backend_version} vs frontend = {frontend_version}"
+        workspace_version, frontend_version,
+        "Versions are not the same: workspace = {workspace_version} vs frontend = {frontend_version}"
     );
 }
 
@@ -18,12 +20,6 @@ fn read_cargo_toml(path: &str) -> toml::Value {
     fs::read_to_string(path).unwrap().parse().unwrap()
 }
 
-fn get_version(cargo_toml: &toml::Value) -> &str {
-    cargo_toml
-        .get("package")
-        .unwrap()
-        .get("version")
-        .unwrap()
-        .as_str()
-        .unwrap()
+fn get_package_version(cargo_toml: &toml::Value) -> &toml::Value {
+    cargo_toml.get("package").unwrap().get("version").unwrap()
 }
