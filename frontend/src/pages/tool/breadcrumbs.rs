ad1e9dcd5ad6ca60ec9e99fb86f4c852d9692929
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-03-17 17:15:27 +0100

    Remove house icon from breadcrumbs

--- frontend/src/pages/tool/breadcrumbs.rs
+++ frontend/src/pages/tool/breadcrumbs.rs
@@ -14,25 +14,6 @@ where
     view! {
       <nav class="flex" aria-label="Breadcrumb">
         <ol role="list" class="flex space-x-4 rounded-md bg-white px-6 shadow">
-          <li class="flex">
-            <div class="flex items-center">
-              <a href="#" class="text-gray-400 hover:text-gray-500">
-                <svg
-                  class="h-5 w-5 flex-shrink-0"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span class="sr-only">"Home"</span>
-              </a>
-            </div>
-          </li>
           { crumbs }
         </ol>
       </nav>
