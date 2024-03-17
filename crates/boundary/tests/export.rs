7faef19a10f027217cef4ef8036a48d10f519b9d
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-03-17 18:21:54 +0100

    Fix boundary tests

--- crates/boundary/tests/export.rs
+++ crates/boundary/tests/export.rs
@@ -1,12 +1,7 @@
 use time::OffsetDateTime;
 use uuid::Uuid;
 
-use klick_boundary::{
-    export_to_string_pretty, import_from_str, AnnualAverageInfluent,
-    CH4ChpEmissionFactorCalcMethod, CH4ChpEmissionFactorScenario, Data, FormData,
-    N2oEmissionFactorCalcMethod, N2oEmissionFactorScenario, OptimizationScenario, PlantProfile,
-    Project, ProjectId, SavedProject, CURRENT_VERSION,
-};
+use klick_boundary::*;
 
 #[test]
 fn export() {
@@ -16,99 +11,105 @@ fn export() {
         ..Default::default()
     };
 
-    // FIXME:
-    // let optimization_scenario = OptimizationScenario {
-    //     n2o_emission_factor: N2oEmissionFactorScenario {
-    //         custom_factor: Some(0.013),
-    //         calculation_method: N2oEmissionFactorCalcMethod::CustomFactor,
-    //     },
-    //     ch4_chp_emission_factor: Some(CH4ChpEmissionFactorScenario {
-    //         custom_factor: Some(0.0345),
-    //         calculation_method: CH4ChpEmissionFactorCalcMethod::MicroGasTurbines,
-    //     }),
-    // };
+    let sensitivity_parameters = SensitivityParameters {
+        n2o_emissions: N2OEmissionsSensitivity {
+            custom_emission_factor: Some(0.013),
+            calculation_method: Some(N2oEmissionFactorCalcMethod::CustomFactor),
+            side_stream_emission_factor: None,
+        },
+        ch4_chp_emissions: CH4ChpEmissionsSensitivity {
+            custom_emission_factor: Some(0.0345),
+            calculation_method: Some(CH4ChpEmissionFactorCalcMethod::MicroGasTurbines),
+        },
+        ch4_sewage_sludge_emissions: SewageSludgeTreatmentEmissionsSensitivity::default(),
+        co2_fossil_emissions: FossilEmissonsSensitivity::default(),
+    };
 
-    // let id = ProjectId(Uuid::new_v4());
-    // let data = ProjectData {
-    //     title: Some("Project".into()),
-    //     plant_profile,
-    //     optimization_scenario,
-    // };
-    // let project = SavedProject {
-    //     id,
-    //     created_at: OffsetDateTime::now_utc(),
-    //     modified_at: None,
-    //     data,
-    // }
-    // .into();
-    // let data = Data { project };
+    let id = ProjectId(Uuid::new_v4());
+    let data = FormData {
+        project_title: Some("Project".into()),
+        plant_profile,
+        sensitivity_parameters,
+        optimization_scenario: Default::default(),
+    };
+    let project = SavedProject {
+        id,
+        created_at: OffsetDateTime::now_utc(),
+        modified_at: None,
+        data,
+    }
+    .into();
+    let data = Data { project };
 
-    // let json_string = export_to_string_pretty(&data);
-    // assert!(json_string.starts_with(&format!("{{\n  \"version\": {CURRENT_VERSION}")));
-    // assert!(json_string.contains("\"wastewater\": 3456.889"));
-    // let json = serde_json::from_str::<serde_json::Value>(&json_string).unwrap();
-    // let project = &json["project"];
+    let json_string = export_to_string_pretty(&data);
+    assert!(json_string.starts_with(&format!("{{\n  \"version\": {CURRENT_VERSION}")));
+    assert!(json_string.contains("\"wastewater\": 3456.889"));
+    let json = serde_json::from_str::<serde_json::Value>(&json_string).unwrap();
+    let project = &json["project"];
 
-    // assert_eq!(project["id"], id.0.to_string());
-    // assert_eq!(project["title"], "Project");
+    assert_eq!(project["id"], id.0.to_string());
+    assert_eq!(project["project_title"], "Project");
 
-    // assert_eq!(
-    //     project["optimization_scenario"]["n2o_emission_factor"]["custom_factor"],
-    //     0.013
-    // );
-    // assert_eq!(
-    //     project["optimization_scenario"]["n2o_emission_factor"]["calculation_method"],
-    //     "custom-factor"
-    // );
+    assert_eq!(
+        project["sensitivity_parameters"]["n2o_emissions"]["custom_emission_factor"],
+        0.013
+    );
+    assert_eq!(
+        project["sensitivity_parameters"]["n2o_emissions"]["calculation_method"],
+        "custom-factor"
+    );
 
-    // assert_eq!(
-    //     project["optimization_scenario"]["ch4_chp_emission_factor"]["custom_factor"],
-    //     0.0345
-    // );
-    // assert_eq!(
-    //     project["optimization_scenario"]["ch4_chp_emission_factor"]["calculation_method"],
-    //     "micro-gas-turbines"
-    // );
+    assert_eq!(
+        project["sensitivity_parameters"]["ch4_chp_emissions"]["custom_emission_factor"],
+        0.0345
+    );
+    assert_eq!(
+        project["sensitivity_parameters"]["ch4_chp_emissions"]["calculation_method"],
+        "micro-gas-turbines"
+    );
 }
 
-// FIXME:
-// #[test]
-// fn roundtrip() {
-//     let plant_profile = PlantProfile {
-//         plant_name: Some("test export".to_string()),
-//         wastewater: Some(3456.889),
-//         influent_average: AnnualAverageInfluent {
-//             total_nitrogen: Some(1.2345000000000000000000000001),
-//             ..Default::default()
-//         },
-//         ..Default::default()
-//     };
-//     let optimization_scenario = OptimizationScenario {
-//         n2o_emission_factor: N2oEmissionFactorScenario {
-//             custom_factor: Some(0.013),
-//             calculation_method: N2oEmissionFactorCalcMethod::Pesimistic,
-//         },
-//         ch4_chp_emission_factor: Some(CH4ChpEmissionFactorScenario {
-//             custom_factor: Some(0.013),
-//             calculation_method: CH4ChpEmissionFactorCalcMethod::GasolineEngine,
-//         }),
-//     };
-//
-//     let id = ProjectId(Uuid::new_v4());
-//     let project = Project::Saved(SavedProject {
-//         id,
-//         created_at: OffsetDateTime::now_utc(),
-//         modified_at: None,
-//         data: ProjectData {
-//             title: Some("Project".into()),
-//             plant_profile,
-//             optimization_scenario,
-//         },
-//     });
-//     let data = Data {
-//         project: project.clone(),
-//     };
-//     let json_string = export_to_string_pretty(&data);
-//     let imported = import_from_str(&json_string).unwrap();
-//     assert_eq!(imported, project);
-// }
+#[test]
+fn roundtrip() {
+    let plant_profile = PlantProfile {
+        plant_name: Some("test export".to_string()),
+        wastewater: Some(3456.889),
+        influent_average: AnnualAverageInfluent {
+            total_nitrogen: Some(1.2345000000000000000000000001),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    let sensitivity_parameters = SensitivityParameters {
+        n2o_emissions: N2OEmissionsSensitivity {
+            custom_emission_factor: Some(0.013),
+            calculation_method: Some(N2oEmissionFactorCalcMethod::Pesimistic),
+            side_stream_emission_factor: None,
+        },
+        ch4_chp_emissions: CH4ChpEmissionsSensitivity {
+            custom_emission_factor: Some(0.013),
+            calculation_method: Some(CH4ChpEmissionFactorCalcMethod::GasolineEngine),
+        },
+        ch4_sewage_sludge_emissions: SewageSludgeTreatmentEmissionsSensitivity::default(),
+        co2_fossil_emissions: FossilEmissonsSensitivity::default(),
+    };
+
+    let id = ProjectId(Uuid::new_v4());
+    let project = Project::Saved(SavedProject {
+        id,
+        created_at: OffsetDateTime::now_utc(),
+        modified_at: None,
+        data: FormData {
+            project_title: Some("Project".into()),
+            plant_profile,
+            sensitivity_parameters,
+            optimization_scenario: Default::default(),
+        },
+    });
+    let data = Data {
+        project: project.clone(),
+    };
+    let json_string = export_to_string_pretty(&data);
+    let imported = import_from_str(&json_string).unwrap();
+    assert_eq!(imported, project);
+}
--- crates/boundary/tests/import.rs
+++ crates/boundary/tests/import.rs
@@ -23,7 +23,7 @@ fn import_v1() {
         project_title: _,
         plant_profile,
         sensitivity_parameters,
-        optimization_scenario,
+        optimization_scenario: _,
     }) = import_from_str(json).unwrap()
     else {
         panic!("expected unsaved project");
@@ -54,9 +54,7 @@ fn import_v1() {
 
     let SewageSludgeTreatment {
         sludge_bags_are_closed,
-        // FIXME: sludge_bags_factor,
         sludge_storage_containers_are_closed,
-        // FIXME: sludge_storage_containers_factor,
         sewage_sludge_for_disposal,
         transport_distance,
         digester_count,
@@ -81,9 +79,19 @@ fn import_v1() {
     assert_eq!(total_power_consumption, Some(2_683_259.0));
 
     assert_eq!(sludge_bags_are_closed, Some(false));
-    // FIXME: assert_eq!(custom_sludge_bags_factor, None);
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_bags,
+        None
+    );
     assert_eq!(sludge_storage_containers_are_closed, Some(false));
-    // FIXME: assert_eq!(custom_sludge_storage_containers_factor, None);
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_storage_containers,
+        None
+    );
     assert_eq!(sewage_sludge_for_disposal, Some(3687.6));
     assert_eq!(transport_distance, Some(47.0));
     assert_eq!(digester_count, None);
@@ -93,276 +101,323 @@ fn import_v1() {
     assert_eq!(operating_materials.caoh2, Some(326.26));
     assert_eq!(operating_materials.synthetic_polymers, Some(23.62));
 
-    // FIXME:
-    // assert_eq!(
-    //     optimization_scenario.n2o_emission_factor.custom_factor,
-    //     Some(1.5)
-    // );
-    // assert_eq!(
-    //     optimization_scenario.n2o_emission_factor.calculation_method,
-    //     N2oEmissionFactorCalcMethod::CustomFactor
-    // );
-    // assert_eq!(optimization_scenario.ch4_chp_emission_factor, None);
+    assert_eq!(
+        sensitivity_parameters.n2o_emissions.custom_emission_factor,
+        Some(1.5)
+    );
+    assert_eq!(
+        sensitivity_parameters.n2o_emissions.calculation_method,
+        Some(N2oEmissionFactorCalcMethod::CustomFactor)
+    );
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
 }
 
-// FIXME:
-// #[test]
-// fn import_v2() {
-//     let json = include_str!("example_data_v2.json");
-//
-//     let Project::Unsaved(ProjectData {
-//         title: _,
-//         plant_profile,
-//         optimization_scenario,
-//     }) = import_from_str(json).unwrap()
-//     else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     let PlantProfile {
-//         plant_name,
-//         population_equivalent,
-//         wastewater,
-//         influent_average,
-//         effluent_average,
-//         energy_consumption,
-//         sewage_sludge_treatment,
-//         operating_materials,
-//         ..
-//     } = plant_profile;
-//
-//     let EnergyConsumption {
-//         sewage_gas_produced,
-//         methane_fraction,
-//         gas_supply,
-//         purchase_of_biogas,
-//         total_power_consumption,
-//         on_site_power_generation,
-//         emission_factor_electricity_mix,
-//         ..
-//     } = energy_consumption;
-//
-//     let SewageSludgeTreatment {
-//         sludge_bags_are_open,
-//         custom_sludge_bags_factor,
-//         sludge_storage_containers_are_open,
-//         custom_sludge_storage_containers_factor,
-//         sewage_sludge_for_disposal,
-//         transport_distance,
-//         digester_count,
-//         ..
-//     } = sewage_sludge_treatment;
-//
-//     assert_eq!(plant_name.as_deref(), Some("Example Plant"));
-//     assert_eq!(population_equivalent, Some(120_000.0));
-//     assert_eq!(wastewater, Some(5_000_000.0));
-//
-//     assert_eq!(influent_average.nitrogen, Some(122.0));
-//     assert_eq!(influent_average.chemical_oxygen_demand, Some(333.0));
-//
-//     assert_eq!(effluent_average.nitrogen, Some(11.76));
-//     assert_eq!(effluent_average.chemical_oxygen_demand, Some(129.0));
-//
-//     assert_eq!(sewage_gas_produced, Some(1_260_000.0));
-//     assert_eq!(methane_fraction, Some(62.0));
-//     assert_eq!(on_site_power_generation, Some(2_250_897.0));
-//     assert_eq!(emission_factor_electricity_mix, Some(468.0));
-//     assert_eq!(gas_supply, Some(500.0));
-//     assert_eq!(purchase_of_biogas, Some(true));
-//     assert_eq!(total_power_consumption, Some(2_683_259.0));
-//
-//     assert_eq!(sludge_bags_are_open, Some(true));
-//     assert_eq!(custom_sludge_bags_factor, None);
-//     assert_eq!(sludge_storage_containers_are_open, Some(true));
-//     assert_eq!(custom_sludge_storage_containers_factor, None);
-//     assert_eq!(sewage_sludge_for_disposal, Some(3687.6));
-//     assert_eq!(transport_distance, Some(47.0));
-//     assert_eq!(digester_count, None);
-//
-//     assert_eq!(operating_materials.fecl3, Some(12.345));
-//     assert_eq!(operating_materials.feclso4, Some(326.0));
-//     assert_eq!(operating_materials.caoh2, Some(326.26));
-//     assert_eq!(operating_materials.synthetic_polymers, Some(23.62));
-//
-//     assert_eq!(
-//         optimization_scenario.n2o_emission_factor.custom_factor,
-//         Some(1.5)
-//     );
-//     assert_eq!(
-//         optimization_scenario.n2o_emission_factor.calculation_method,
-//         N2oEmissionFactorCalcMethod::CustomFactor
-//     );
-//
-//     assert_eq!(optimization_scenario.ch4_chp_emission_factor, None);
-// }
-//
-// #[test]
-// fn import_v3() {
-//     let json = include_str!("example_data_v3.json");
-//
-//     let Project::Unsaved(project) = import_from_str(json).unwrap() else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     assert_eq!(
-//         project
-//             .optimization_scenario
-//             .n2o_emission_factor
-//             .calculation_method,
-//         N2oEmissionFactorCalcMethod::TuWien2016
-//     );
-//
-//     assert_eq!(project.optimization_scenario.ch4_chp_emission_factor, None);
-// }
-//
-// #[test]
-// fn import_v5() {
-//     let json = include_str!("example_data_v5.json");
-//     let Project::Unsaved(project) = import_from_str(json).unwrap() else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     assert_eq!(
-//         project
-//             .optimization_scenario
-//             .n2o_emission_factor
-//             .calculation_method,
-//         N2oEmissionFactorCalcMethod::Ipcc2019
-//     );
-//
-//     assert_eq!(project.optimization_scenario.ch4_chp_emission_factor, None);
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_bags_are_open,
-//         Some(true)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_storage_containers_are_open,
-//         Some(true)
-//     );
-// }
-//
-// #[test]
-// fn import_v6() {
-//     let json = include_str!("example_data_v6.json");
-//     let Project::Unsaved(project) = import_from_str(json).unwrap() else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     assert_eq!(
-//         project
-//             .optimization_scenario
-//             .n2o_emission_factor
-//             .calculation_method,
-//         N2oEmissionFactorCalcMethod::Ipcc2019
-//     );
-//
-//     assert_eq!(project.optimization_scenario.ch4_chp_emission_factor, None);
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_bags_are_open,
-//         Some(true)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_storage_containers_are_open,
-//         Some(true)
-//     );
-// }
-//
-// #[test]
-// fn import_v7() {
-//     let json = include_str!("example_data_v7.json");
-//     let Project::Unsaved(project) = import_from_str(json).unwrap() else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     assert_eq!(
-//         project
-//             .optimization_scenario
-//             .n2o_emission_factor
-//             .calculation_method,
-//         N2oEmissionFactorCalcMethod::Ipcc2019
-//     );
-//
-//     assert_eq!(project.optimization_scenario.ch4_chp_emission_factor, None);
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .custom_sludge_bags_factor,
-//         Some(1.12)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .custom_sludge_storage_containers_factor,
-//         Some(1.13)
-//     );
-// }
-//
-// #[test]
-// fn import_v8() {
-//     let json = include_str!("example_data_v8.json");
-//     let Project::Unsaved(project) = import_from_str(json).unwrap() else {
-//         panic!("expected unsaved project");
-//     };
-//
-//     assert_eq!(
-//         project
-//             .optimization_scenario
-//             .n2o_emission_factor
-//             .calculation_method,
-//         N2oEmissionFactorCalcMethod::Ipcc2019
-//     );
-//
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .influent_average
-//             .total_organic_carbohydrates,
-//         Some(101.99)
-//     );
-//     assert_eq!(
-//         project.plant_profile.side_stream_treatment.total_nitrogen,
-//         Some(101.4)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_bags_are_open,
-//         Some(true)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_bags_are_open_recommendation,
-//         Some(true)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_storage_containers_are_open,
-//         Some(true)
-//     );
-//     assert_eq!(
-//         project
-//             .plant_profile
-//             .sewage_sludge_treatment
-//             .sludge_storage_containers_are_open_recommendation,
-//         Some(true)
-//     );
-// }
+#[test]
+fn import_v2() {
+    let json = include_str!("example_data_v2.json");
+
+    let Project::Unsaved(FormData {
+        project_title: _,
+        plant_profile,
+        sensitivity_parameters,
+        optimization_scenario: _,
+    }) = import_from_str(json).unwrap()
+    else {
+        panic!("expected unsaved project");
+    };
+
+    let PlantProfile {
+        plant_name,
+        population_equivalent,
+        wastewater,
+        influent_average,
+        effluent_average,
+        energy_consumption,
+        sewage_sludge_treatment,
+        operating_materials,
+        ..
+    } = plant_profile;
+
+    let EnergyConsumption {
+        sewage_gas_produced,
+        methane_fraction,
+        gas_supply,
+        purchase_of_biogas,
+        total_power_consumption,
+        on_site_power_generation,
+        emission_factor_electricity_mix,
+        ..
+    } = energy_consumption;
+
+    let SewageSludgeTreatment {
+        sludge_bags_are_closed,
+        sludge_storage_containers_are_closed,
+        sewage_sludge_for_disposal,
+        transport_distance,
+        digester_count,
+        ..
+    } = sewage_sludge_treatment;
+
+    assert_eq!(plant_name.as_deref(), Some("Example Plant"));
+    assert_eq!(population_equivalent, Some(120_000.0));
+    assert_eq!(wastewater, Some(5_000_000.0));
+
+    assert_eq!(influent_average.total_nitrogen, Some(122.0));
+    assert_eq!(influent_average.chemical_oxygen_demand, Some(333.0));
+
+    assert_eq!(effluent_average.total_nitrogen, Some(11.76));
+    assert_eq!(effluent_average.chemical_oxygen_demand, Some(129.0));
+
+    assert_eq!(sewage_gas_produced, Some(1_260_000.0));
+    assert_eq!(methane_fraction, Some(62.0));
+    assert_eq!(on_site_power_generation, Some(2_250_897.0));
+    assert_eq!(emission_factor_electricity_mix, Some(468.0));
+    assert_eq!(gas_supply, Some(500.0));
+    assert_eq!(purchase_of_biogas, Some(true));
+    assert_eq!(total_power_consumption, Some(2_683_259.0));
+
+    assert_eq!(sludge_bags_are_closed, Some(false));
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_bags,
+        None
+    );
+    assert_eq!(sludge_storage_containers_are_closed, Some(false));
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_storage_containers,
+        None
+    );
+    assert_eq!(sewage_sludge_for_disposal, Some(3687.6));
+    assert_eq!(transport_distance, Some(47.0));
+    assert_eq!(digester_count, None);
+
+    assert_eq!(operating_materials.fecl3, Some(12.345));
+    assert_eq!(operating_materials.feclso4, Some(326.0));
+    assert_eq!(operating_materials.caoh2, Some(326.26));
+    assert_eq!(operating_materials.synthetic_polymers, Some(23.62));
+
+    assert_eq!(
+        sensitivity_parameters.n2o_emissions.custom_emission_factor,
+        Some(1.5)
+    );
+    assert_eq!(
+        sensitivity_parameters.n2o_emissions.calculation_method,
+        Some(N2oEmissionFactorCalcMethod::CustomFactor)
+    );
+
+    assert_eq!(
+        sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
+}
+
+#[test]
+fn import_v3() {
+    let json = include_str!("example_data_v3.json");
+
+    let Project::Unsaved(project) = import_from_str(json).unwrap() else {
+        panic!("expected unsaved project");
+    };
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .n2o_emissions
+            .calculation_method,
+        Some(N2oEmissionFactorCalcMethod::TuWien2016)
+    );
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
+}
+
+#[test]
+fn import_v5() {
+    let json = include_str!("example_data_v5.json");
+    let Project::Unsaved(project) = import_from_str(json).unwrap() else {
+        panic!("expected unsaved project");
+    };
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .n2o_emissions
+            .calculation_method,
+        Some(N2oEmissionFactorCalcMethod::Ipcc2019)
+    );
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_bags_are_closed,
+        Some(false)
+    );
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_storage_containers_are_closed,
+        Some(false)
+    );
+}
+
+#[test]
+fn import_v6() {
+    let json = include_str!("example_data_v6.json");
+    let Project::Unsaved(project) = import_from_str(json).unwrap() else {
+        panic!("expected unsaved project");
+    };
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .n2o_emissions
+            .calculation_method,
+        Some(N2oEmissionFactorCalcMethod::Ipcc2019)
+    );
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_bags_are_closed,
+        Some(false)
+    );
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_storage_containers_are_closed,
+        Some(false)
+    );
+}
+
+#[test]
+fn import_v7() {
+    let json = include_str!("example_data_v7.json");
+    let Project::Unsaved(project) = import_from_str(json).unwrap() else {
+        panic!("expected unsaved project");
+    };
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .n2o_emissions
+            .calculation_method,
+        Some(N2oEmissionFactorCalcMethod::Ipcc2019)
+    );
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_chp_emissions
+            .custom_emission_factor,
+        None
+    );
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_bags,
+        Some(1.12)
+    );
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .ch4_sewage_sludge_emissions
+            .emission_factor_sludge_storage_containers,
+        Some(1.13)
+    );
+}
+
+#[test]
+fn import_v8() {
+    let json = include_str!("example_data_v8.json");
+    let Project::Unsaved(project) = import_from_str(json).unwrap() else {
+        panic!("expected unsaved project");
+    };
+
+    assert_eq!(
+        project
+            .sensitivity_parameters
+            .n2o_emissions
+            .calculation_method,
+        Some(N2oEmissionFactorCalcMethod::Ipcc2019)
+    );
+
+    assert_eq!(
+        project
+            .plant_profile
+            .influent_average
+            .total_organic_carbohydrates,
+        Some(101.99)
+    );
+
+    assert_eq!(
+        project.plant_profile.side_stream_treatment.total_nitrogen,
+        Some(101.4)
+    );
+
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_bags_are_closed,
+        Some(false)
+    );
+
+    assert_eq!(
+        project
+            .plant_profile
+            .sewage_sludge_treatment
+            .sludge_storage_containers_are_closed,
+        Some(false)
+    );
+
+    assert_eq!(
+        project
+            .optimization_scenario
+            .sewage_sludge_treatment
+            .sludge_bags_are_closed,
+        Some(true)
+    );
+
+    assert_eq!(
+        project
+            .optimization_scenario
+            .sewage_sludge_treatment
+            .sludge_storage_containers_are_closed,
+        Some(true)
+    );
+}
