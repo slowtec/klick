6e6f8bb2cfc11d9a0ab71e92f0eb1955d1a2f2ca
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-03-28 12:13:21 +0100

    Fix project menu

--- frontend/src/pages/tool/project_menu.rs
+++ frontend/src/pages/tool/project_menu.rs
@@ -51,25 +51,8 @@ pub fn ProjectMenu(
               //     To: "transform opacity-0 scale-95"
               // -->
               <Show when = move || is_open.get() >
-                <div class="absolute right-0 z-10 mt-2 w-64 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                <div class="absolute right-0 z-10 mt-2 w-72 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                   <div class="py-1" role="none">
-                    <Entry
-                      label = "Werte laden"
-                      on:click = move |_| {
-                        save.call(());
-                        is_open.set(false);
-                      }
-                      disabled = Signal::derive(move|| !logged_in.get())
-                      disabled_text = "nur mit Login"
-                      icon = icons::CloudArrowDown
-                    />
-                    <Entry
-                      label = "Werte speichern"
-                      href = Page::Projects.path()
-                      disabled = Signal::derive(move|| !logged_in.get())
-                      disabled_text = "nur mit Login"
-                      icon = icons::CloudArrowUp
-                    />
                     <Entry
                       label = "Werte zurücksetzen"
                       on:click = move |_| {
@@ -135,6 +118,25 @@ pub fn ProjectMenu(
                           fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                         </svg>
+                      }
+                    />
+                  </Section>
+                  <Section>
+                    <Entry
+                      label = "Projekt aus Online-Speicher laden"
+                      disabled = Signal::derive(move|| !logged_in.get())
+                      disabled_text = "nur mit Login"
+                      icon = icons::CloudArrowUp
+                      href = Page::Projects.path()
+                    />
+                    <Entry
+                      label = "Projekt online speichern"
+                      disabled = Signal::derive(move|| !logged_in.get())
+                      disabled_text = "nur mit Login"
+                      icon = icons::CloudArrowDown
+                      on:click = move |_| {
+                        save.call(());
+                        is_open.set(false);
                       }
                     />
                   </Section>
