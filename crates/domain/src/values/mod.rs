4f67fda7530364d53d28b2c264440b4a39b49d70
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-05-19 13:57:01 +0200

    Move value IDs into domain layer

--- crates/domain/src/values/mod.rs
+++ crates/domain/src/values/mod.rs
@@ -3,6 +3,8 @@ mod co2_equivalents;
 mod emission_factors;
 mod emission_influencing_values;
 
+pub mod value_ids;
+
 pub use self::{
     calculation_outcome::*, co2_equivalents::*, emission_factors::*, emission_influencing_values::*,
 };
