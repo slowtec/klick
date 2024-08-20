6be5a4bdc587b9f58311772ed3c5dc002d9101c9
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-08-20 03:19:46 +0200

    Set log level to 'info' in release mode

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,8 +2,14 @@ use leptos::{mount_to_body, view};
 
 use klick_frontend::App;
 
+#[cfg(debug_assertions)]
+const LOG_LEVEL: log::Level = log::Level::Debug;
+
+#[cfg(not(debug_assertions))]
+const LOG_LEVEL: log::Level = log::Level::Info;
+
 pub fn main() {
-    _ = console_log::init_with_level(log::Level::Debug); // TODO: use 'Info' in release mode
+    _ = console_log::init_with_level(LOG_LEVEL);
     console_error_panic_hook::set_once();
     log::info!("Start web application");
     mount_to_body(|| {
