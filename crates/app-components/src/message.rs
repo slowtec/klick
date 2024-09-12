aabf20843d029fa79c4cc99ce413c3e91c38864e
Joachim Schiele <js@lastlog.de>
2024-09-12 23:07:11 +0000

    make SuccessMessage cancelable

--- crates/app-components/src/message.rs
+++ crates/app-components/src/message.rs
@@ -26,8 +26,9 @@ pub fn InfoMessage(message: &'static str) -> impl IntoView {
 #[component]
 #[must_use]
 pub fn SuccessMessage(message: &'static str) -> impl IntoView {
+    let show: RwSignal<bool> = RwSignal::new(true);
     view! {
-      <div class="rounded-md bg-green-50 p-4">
+      <div class="rounded-md bg-green-50 p-4" style = move || if show.get() { None } else { Some("display:none;") }>
         <div class="flex">
           <div class="flex-shrink-0">
             <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -39,7 +40,7 @@ pub fn SuccessMessage(message: &'static str) -> impl IntoView {
           </div>
           <div class="ml-auto pl-3">
             <div class="-mx-1.5 -my-1.5">
-              <button type="button" class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+              <button type="button" on:click = move |_| show.set(false) class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
                 <span class="sr-only">Dismiss</span>
                 <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
